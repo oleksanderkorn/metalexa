990240fb47206142340af69659169e4c8036ea45
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2022-03-16 14:15:23 +0100

    Add text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/variables#:~:text=Variables%20represent%20storage%20locations.,always%20of%20the%20appropriate%20type.">
+        <header className="App-header">
+          <h1>
+            Переменная это хранилище, область памяти, в него можно положить
+            число строку и тд и использовать его и изменять
+          </h1>
+        </header>
+      </a>
     </div>
   );
 }
