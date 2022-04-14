d99d00157a217164e14fa530a0bba414c52a87cf
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2022-04-14 22:51:49 +0200

    Update text.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ function App() {
     <div className="App">
       <a href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/variables#:~:text=Variables%20represent%20storage%20locations.,always%20of%20the%20appropriate%20type.">
         <header className="App-header">
-          <h1>
-            Переменная это хранилище, область памяти, в него можно положить
-            число строку и тд и использовать его и изменять
-          </h1>
+          <h1>METALEXA</h1>
         </header>
       </a>
     </div>
